89d6eee7a39cf539c3df737a68e1d4fc0c0eed1c
Arthur <lilacell.ag@email.com>
2026-04-27 14:11:37 +0300

    Add types.kt with all basic Kotlin data types

---
+++ src/main/kotlin/types.kt
@@ -0,0 +1,22 @@
+fun main() {
+    val sampleByte: Byte = 42
+    val sampleShort: Short = 300
+    val sampleInt: Int = 100000
+    val sampleLong: Long = 9999999999L
+    val sampleFloat: Float = 3.14f
+    val sampleDouble: Double = 2.71828
+    val sampleBoolean: Boolean = true
+    val sampleChar: Char = 'Z'
+    val sampleString: String = "Kotlin"
+
+    println(sampleByte)
+    println(sampleShort)
+    println(sampleInt)
+    println(sampleLong)
+    println(sampleFloat)
+    println(sampleDouble)
+    println(sampleBoolean)
+    println(sampleChar)
+    println(sampleString)
+
+}
