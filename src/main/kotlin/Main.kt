12dbd3521d9aace3ac6d6d6aa6538b91d784135e
Arthur <lilacell.ag@email.com>
2026-04-27 14:48:33 +0300

    Add println with string templates

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,4 +8,10 @@ fun main() {
     val isEmployed: Boolean = false
     val osCount: Byte = 3
     val studentId: Short = 1024
+
+    println("Досье разработчика: ${developerName} (инициал ${nameInitial}).")
+    println("Ему ${ageYears} лет, и он проживает в городе с населением ${cityPopulation} человек.")
+    println("Рост составляет ${heightMeters} м, а средний балл зачётки — ${gpaScore}.")
+    println("Официально трудоустроен: ${isEmployed}.")
+    println("На личном ноутбуке установлено ${osCount} операционные системы, а его студенческий билет имеет номер ${studentId}.")
 }
